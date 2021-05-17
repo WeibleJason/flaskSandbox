12710d6e5d9b2ea5fd0de8285947f043168806bc
Jason Weible <weible.jason@gmail.com>
2021-05-17 12:36:55 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import base64
+from io import BytesIO
+
+from flask import Flask
+from matplotlib.figure import Figure
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    # Generate the figure **without using pyplot**.
+    fig = Figure()
+    ax = fig.subplots()
+    ax.plot([1, 2])
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    # Embed the result in the html output
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return f"<img src='data:image/png;base64,{data}'/>"
+
+
+    return '<h1>Hello World!</h1>'
+
+
+app.add_url_rule('/' 'index', index)
+
